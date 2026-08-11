8cf598ace7fda528075821cecfa489649705193f
UGREEN USER <n.aliaga@proton.me>
2026-08-11 20:09:44 +0200

    WebHook Change V2

--- discord_bot.py
+++ discord_bot.py
@@ -327,8 +327,7 @@ class GGEAssistantBot(commands.Bot):
     async def vote_handler(self, request):
         # 1. On vérifie le mot de passe (sécurité)
         auth_header = request.headers.get("Authorization")
-        # ⚠️ Tu mettras ce même mot de passe sur le site de Top.gg !
-        if auth_header != "[SECRET_TOPGG_8]": 
+        if auth_header != "[SECRET_TOPGG_7]": 
             return web.Response(status=401, text="Accès refusé.")
 
         # 2. On lit les données envoyées par Top.gg
